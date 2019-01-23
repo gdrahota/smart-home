61c1e196900f6edac2b06f2e4ead22eb7a3484e3
Guido Drahota <guido@drahota.de>
2019-01-23 09:19:06 +0100

    backend: control-data-point service: method find() added

--- backend/src/services/control-data-points.js
+++ backend/src/services/control-data-points.js
@@ -4,12 +4,14 @@ const collection = 'control-data-points'
 
 const getAll = () => Repository(collection).getAll()
 const findOne = searchObj => Repository(collection).findOne(searchObj)
+const find = searchObj => Repository(collection).find(searchObj)
 const remove = id => Repository(collection).remove(id)
 const removeControlDataPoint = (control, endPoint) => Repository(collection).removeControlDataPoint(control, endPoint)
 const upsert = item => Repository(collection).upsert(item, { control: item.control, endPoint: item.endPoint })
 
 export const ControlDataPointService = {
   getAll,
+  find,
   findOne,
   remove,
   upsert,
