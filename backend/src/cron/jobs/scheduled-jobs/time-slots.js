2d14d9ee103f27ee162022ba0e51b1350a742fe3
Guido Drahota <guido@drahota.de>
2019-02-05 11:18:24 +0100

    backend: time slot scheduler enhanced: if the previous time slot had the same value as the current one then any command execution will be skipped

--- backend/src/cron/jobs/scheduled-jobs/time-slots.js
+++ backend/src/cron/jobs/scheduled-jobs/time-slots.js
@@ -11,26 +11,35 @@ export class TimeSlotJob {
     }
 
     const now = moment()
+    const fifteenMinutesAgo = moment().subtract(15, 'minutes')
 
     // this type job can only be run every 15 minutes (0, 15, 30, 45 minute of the hour)
     if (now.minutes() % 15 !== 0) {
       return null
     }
 
+    const highOrLowNow = this.getTimeSlot(now)
+    const highOrLowQuarterAnHourAgo = this.getTimeSlot(fifteenMinutesAgo)
+
+    // if the time slot is not set (corrupt data) skip it
+    if (highOrLowNow === undefined) {
+      return null
+    }
+
+    // if there isn't any change to the previous time slot then don't do anything
+    if (highOrLowNow === highOrLowQuarterAnHourAgo) {
+      return null
+    }
+
+    return highOrLowNow ? 'maxValue' : 'minValue'
+  }
+
+  getTimeSlot (now) {
     // Mo = 1, Sa = 6, Su = 7
     const dayOfTheWeek = now.day() > 0 ? now.day() : 7
     const hour = now.hour()
     const quarter = Math.floor(now.minutes() / 15)
-
     const attrName = 'd' + dayOfTheWeek + '-h' + hour + '-t' + quarter
-
-    const highOrLow = this.job.timeSlots[attrName]
-    console.log(attrName, highOrLow)
-
-    if (highOrLow === undefined) {
-      return null
-    }
-
-    return highOrLow ? 'maxValue' : 'minValue'
+    return this.job.timeSlots[attrName]
   }
 }
