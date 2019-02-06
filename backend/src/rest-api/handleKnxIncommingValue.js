006cf1e2741f516262c12316b15ad4cd383d950c
Guido Drahota <guido@drahota.de>
2019-02-06 08:43:17 +0100

    backend: bug fixed: docs in knx-values will now be automatically deleted

--- backend/src/rest-api/handleKnxIncommingValue.js
+++ backend/src/rest-api/handleKnxIncommingValue.js
@@ -33,7 +33,7 @@ export const handleKnxValue = async doc => {
       }
 
       await DataPointService.update(update)
-      await ValuesFromKnxService.remove(dataPoint._id)
+      await ValuesFromKnxService.remove(valueFromKnx._id)
 
       let query = { dataPoint: dataPoint._id }
 
